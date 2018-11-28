9839ee4c5fcaebc137b0b3cf18dde4007f214b82
Fábio Pimentel <fabiolp4@gmail.com>
2018-11-28 02:23:26 -0200

    Initial controller and data - Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+500.times do
+    Post.create!(title: Faker::Movie.quote, body:Faker::Lorem.paragraph(10))
+end
